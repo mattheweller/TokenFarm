798941619ed7b9a336b16d11ef2c55b15911116f
mattheweller <mattcjeller@gmail.com>
2022-01-19 20:51:22 +0000

    Move over files

---
+++ front_end/src/components/index.ts
@@ -0,0 +1,4 @@
+export { useStakeTokens } from "../hooks/useStakeTokens"
+export { ConnectionRequiredMsg } from "./ConnectionRequiredMsg"
+export { SliderInput } from "./SliderInput"
+export { BalanceMsg } from "./BalanceMsg"
