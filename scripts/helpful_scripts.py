b9ad3dccf149c2c6a5531584860792adfde79e93
mattheweller <mattcjeller@gmail.com>
2022-01-20 10:50:34 +0000

    Add issue_tokens() to helpful scripts and main() file to call

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -122,10 +122,17 @@ def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_PRICE_FEED_VALUE):
 
 
 def issue_tokens():
-    """
-    Once you have users that have staked tokens, you can call this function.
-    Note that it relies on get_contract, so be mindful to correctly configure
-    your Token Farm contract into brownie-config.yaml as well as the contract_to_mock dict
+    """You can call this function once you have deployed your TokenFarm contract to a live network
+    and have users that have staked tokens.
+    
+    Note that it relies on get_contract, so be mindful to correctly configure your Token Farm contract
+    into brownie-config.yaml as well as the contract_to_mock dict as described in the get_contract docstring
+
+    Run this function with this command: `brownie run scripts/issue_tokens.py --network kovan`
+
+        This function will:
+            - Print your account address and deployed TokenFarm contract address to confirm that you're using the right ones
+            - Call issueTokens on your deployed TokenFarm contract to issue the DAPP token reward to your users
     """
     account = get_account()
     print(f"Issue Tokens called by: {account}")
--- scripts/issue_tokens.py
+++ scripts/issue_tokens.py
@@ -1,5 +1,6 @@
 from scripts.helpful_scripts import issue_tokens
 
-
 def main():
     issue_tokens()
+
+    
